1da48a6c721dcbf0393ab6ca3743a6bd364e5e7a
endrik2700 <42510329+endrik2700@users.noreply.github.com>
2019-10-31 20:23:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hi enter the word to check for as palindrome");
+        string check = Console.ReadLine();
+        char[] arr1 = check.ToCharArray();
+        char[] arr2 = arr1.Reverse().ToArray();
+        string stringToCompare = String.Concat(arr2);
+       Console.WriteLine( String.Equals(check,stringToCompare)? "This is a palindrome": "This is not a palindrome");
+        Console.ReadLine();
+
+
+    }
+    
+}
